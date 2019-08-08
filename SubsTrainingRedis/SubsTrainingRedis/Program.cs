4dbf0607f3dfc133f184fa5e409984de449c22cf
Logon Aluno <logonaluno@local.com>
2019-08-07 21:28:04 -0300

    First commit

---
+++ SubsTrainingRedis/SubsTrainingRedis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SubsTrainingRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
